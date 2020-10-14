9d83ea4824d0d24e17e976342ebdbeaa05b5ca2e
heriberto.torres <eddietorres622@gmail.com>
2020-10-13 22:27:34 -0400

    added build

---
+++ build/precache-manifest.da916969002540097981621ebc73de80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b982d941ceb3c26a229ee5de73a28a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0047d7697dd7142906b3",
+    "url": "/static/css/main.eb3e9eb7.chunk.css"
+  },
+  {
+    "revision": "1ada86ee16adfb334fa8",
+    "url": "/static/js/2.be078519.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.be078519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0047d7697dd7142906b3",
+    "url": "/static/js/main.7b59be82.chunk.js"
+  },
+  {
+    "revision": "439ae095c7fe9cd37d99",
+    "url": "/static/js/runtime-main.0ea695f4.js"
+  }
+]);
